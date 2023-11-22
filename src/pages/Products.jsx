0f1fcd47b0802f326fa9dffcf2ab4175b3f762dc
M Zain Ali Shan <zain@revolvinggames.com>
2023-11-22 18:37:58 +0500

    front end

---
+++ src/pages/Products.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { FaPlus } from "react-icons/fa";
+import { DisplayAllProducts, Form, Filters } from "../components";
+import { Link, useLoaderData } from "react-router-dom";
+import axios from "axios";
+import { useProductsContext } from "../context/ProductsProvider";
+const REMOTE_SERVER = "https://api.futurafinds.com"
+
+export const loader = async () => {
+  try {
+    const { data } = await axios.get(`${REMOTE_SERVER}/api/v1/products`, {
+      withCredentials: true,
+    });
+    return data;
+  } catch (error) {
+    return error?.response?.data?.msg;
+  }
+};
+
+const AllProducts = () => {
+  const { user } = useProductsContext();
+  const { products } = useLoaderData();
+
+  return (
+    <div className="products">
+      <div className="products-sub-container">
+        <h1>All Products</h1>
+        <div className="plus-icon-container">
+          <button>
+            {user?.userRole === "admin" && (
+              <Link to="/createProduct" className=" btn create-btn">
+                <span>Add new Product</span>
+                <FaPlus className="plus-icon" />
+              </Link>
+            )}
+          </button>
+        </div>
+        <Form />
+        <Filters />
+        <DisplayAllProducts products={products} />
+      </div>
+    </div>
+  );
+};
+
+export default AllProducts;
